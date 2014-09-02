c4eacdb90004df072cbe27b7ba9df4ab30e238ab
merlex <merlex85@gmail.com>
2014-09-03 02:38:51 +0400

    remove editinplace settings

--- testwork/settings.py
+++ testwork/settings.py
@@ -87,17 +87,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "../static"),
-)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
-
-## Inplace edit
-ADAPTOR_INPLACEEDIT_EDIT = 'myapp.perms.MyAdaptorEditInline'
-INPLACEEDIT_DISABLE_CLICK = False
-THUMBNAIL_DEBUG = True
-INPLACEEDIT_EVENT = "click"
